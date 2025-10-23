851e1bb5c23800d70bddcfd9be09a198a624edcd
DecFox <33030671+DecFox@users.noreply.github.com>
2025-10-23 14:24:47 +0530

    debug: try passing in goflag tag to support explicit build tags

--- cmd/gomobile/bind_androidapp.go
+++ cmd/gomobile/bind_androidapp.go
@@ -32,6 +32,8 @@ func goAndroidBind(gobind string, pkgs []*packages.Package, targets []targetInfo
 	)
 	cmd.Env = append(cmd.Env, "GOOS=android")
 	cmd.Env = append(cmd.Env, "CGO_ENABLED=1")
+	cmd.Env = append(cmd.Env, "GOFLAGS=-tags=android")
+	
 	if len(buildTags) > 0 {
 		cmd.Args = append(cmd.Args, "-tags="+strings.Join(buildTags, ","))
 	}
